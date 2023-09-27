98a9dc967f351ad042a2cb54d678c8d53653d3f2
Benício Oliveira <boliveira@tecgraf.puc-rio.br>
2023-09-27 10:36:48 -0300

    - add vercel analytics component - update readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.scss";
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from "next";
 
 import { Inter_Tight, Nanum_Pen_Script } from "next/font/google";
-import type { Metadata } from "next";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -36,6 +37,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
